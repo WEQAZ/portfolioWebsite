c5902c3fb0efaaab55b137f8fe515cb0d14024a4
Piyawut <62786101+WEQAZ@users.noreply.github.com>
2024-06-23 20:47:46 +0700

    add picture header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    <main className="mx-auto py-4 flex min-h-screen flex-col items-center justify-between p-24 bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-bold">
+            Hello, I'm Ice
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet eum
+            quidem enim reprehenderit maxime, iure veniam suscipit! Sed quos,
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 mt-3 rounded-full bg-transparent hover:bg-state-800 text-white border border-white">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-10 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/icePic.png"
+              alt="ice Image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
