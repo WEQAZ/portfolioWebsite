36087fb2b6fbfef913ba7a21d41c58629cef7e78
Piyawut <62786101+WEQAZ@users.noreply.github.com>
2024-06-24 20:13:14 +0700

    Add dynamic typing animation to hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,23 +1,42 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-
+import { TypeAnimation } from "react-type-animation";
+import useRef from "react";
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-bold">
-            Hello, I'm Ice
+            <span className="">Hello, I'm </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Piyawut",
+                1500,
+                "Web Developer",
+                1500,
+                "Cyber Security",
+                1500,
+                "Software Engineer",
+                1500,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet eum
             quidem enim reprehenderit maxime, iure veniam suscipit! Sed quos,
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-black">
               Hire Me
             </button>
-            <button className="px-6 py-3 mt-3 rounded-full bg-transparent hover:bg-state-800 text-white border border-white">
+            <button className="px-6 py-3 mt-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-state-800 text-white border border-white">
               Download CV
             </button>
           </div>
